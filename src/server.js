911daee4fc79b837ed72e54c075f65a90ebc6a19
shadowfox38 <akhil3800@gmail.com>
2020-03-16 20:37:07 +0530

    url for testing logs

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const url = require('url');
+const path = require('path');
 const port = process.env.PORT || 80;
 app.use('/static',express.static('../public'));
 app.use(express.static('../public'));
+app.use(express.static('../data'));
 app.get('/',function(req,res)
 {
   res.sendFile('index.html');
+});
+app.get('*',function(req,res)
+{
+  var q = url.parse(req.url,true);
+  var filepath = q.pathname;
+  res.sendFile('messages.txt');
 });
 app.post('/getdata',function(req,res)
 {
